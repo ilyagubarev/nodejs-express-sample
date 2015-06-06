0dc28cb892f9c54d3cb2b8e4ee5265bdc6ec2ad9
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-07 01:32:19 +0300

    jade dependency added, example view middleware added

--- modules/application.js
+++ modules/application.js
@@ -4,6 +4,16 @@ express()
 
     .use('/resources/static', express.static('modules/resources/static'))
 
+    .set('view engine', 'jade')
+
+    .set('views', 'modules/resources/templates')
+
+    .get('/resources/templates/:template', function (request, response) {
+      response.render(request.params.template, {
+
+      });
+    })
+
     .get('/', function (request, response) {
       response.send('Home page');
     })
