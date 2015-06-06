c60d1bd94b2b3b73b10fb4af3269ff08780bdc60
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-07 01:01:54 +0300

    npm dependency version specified, static resources middleware added

--- modules/application.js
+++ modules/application.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 express()
 
+	.use('/resources/static', express.static('resources/static'));
+
     .get('/', function (request, response) {
       response.send('Home page');
     })
