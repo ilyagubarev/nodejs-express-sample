fb5fc65536998167f3bf5729aabec405d8e83704
Ilya Gubarev <ilya.gubarev@gmail.com>
2015-06-06 15:34:32 +0300

    Update application.js

--- application.js
+++ application.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var app = express();
 
-var application = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-application.get('/', function (request, response) {
+app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-module.exports = application;
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
